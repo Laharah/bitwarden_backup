4e33b77d455be548fcf35c1fbc606e8f08b636f7
laharah <laharah22@gmail.com>
2021-02-17 17:17:56 -0800

    Migrate to bitwarden with attachment download

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='lpass-backup',
+    name='bw-backup',
     version='1.0.0',
     url='',
     license='MIT',
     author='laharah',
     author_email='laharah22+ca@gmail.com',
-    description='Script to download an encrypted backup of a LastPass vault.',
+    description='Script to download an encrypted backup of a BitWarden vault with Attachments.',
     install_requires=[
         'docopt >= 0.5.0',
         'pynentry >= 0.1.0',
@@ -15,6 +15,6 @@ setup(
     dependency_links=[
         'git+https://github.com/Laharah/pynentry#egg=pynentry-0.1.0',
         ],
-    scripts=['lpass-backup'],
+    scripts=['bw-backup'],
 
 )
